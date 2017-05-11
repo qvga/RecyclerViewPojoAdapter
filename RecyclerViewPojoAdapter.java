3b8033693ed239b4715574052c4a9d3c12d27fad
Jakob Eriksson <jakob.eriksson@live.com>
2017-05-11 20:40:32 +0200

    Create RecyclerViewPojoAdapter.java

---
+++ RecyclerViewPojoAdapter.java
@@ -0,0 +1,90 @@
+import android.os.AsyncTask;
+import android.support.annotation.Nullable;
+import android.support.v7.util.DiffUtil;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SimpleItemAnimator;
+import android.view.View;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+
+public abstract class RecyclerViewPojoAdapter<T, VH extends RecyclerViewPojoAdapter.ViewHolder> extends RecyclerView.Adapter<VH> {
+
+    protected RecyclerView recyclerView;
+    private List<T> adapterContent = new ArrayList<>();
+
+    public RecyclerViewPojoAdapter(RecyclerView recyclerView) {
+        super();
+        this.recyclerView = recyclerView;
+    }
+
+
+    public void switchData(@Nullable final List<T> newAdapterContent) {
+
+        if (newAdapterContent == null) {
+            notifyItemRangeRemoved(0, adapterContent.size());
+            adapterContent.clear();
+            return;
+        }
+
+        new AsyncTask<Void, Void, DiffUtil.DiffResult>() {
+
+            @Override
+            protected DiffUtil.DiffResult doInBackground(Void... params) {
+
+                return DiffUtil.calculateDiff(new DiffUtil.Callback() {
+
+                    @Override
+                    public int getOldListSize() {
+                        return adapterContent.size();
+                    }
+
+                    @Override
+                    public int getNewListSize() {
+                        return newAdapterContent.size();
+                    }
+
+                    @Override
+                    public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+                        return Objects.equals(adapterContent.get(oldItemPosition), newAdapterContent.get(newItemPosition));
+                    }
+
+                    @Override
+                    public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+                        return Objects.equals(adapterContent.get(oldItemPosition), newAdapterContent.get(newItemPosition));
+                    }
+
+
+                });
+            }
+
+            @Override
+            protected void onPostExecute(DiffUtil.DiffResult diffResult) {
+
+                adapterContent = newAdapterContent;
+                diffResult.dispatchUpdatesTo(RecyclerViewPojoAdapter.this);
+
+
+            }
+
+        }.execute();
+
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return adapterContent.size();
+    }
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+
+}
